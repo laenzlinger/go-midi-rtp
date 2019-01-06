a72f6c4b2c5c55902fd9d5b79f33143e6ce00113
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2019-01-06 16:45:09 +0100

    assertNoError

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/go-test/deep v1.0.1
 	github.com/grandcat/zeroconf v0.0.0-20181220215047-ce4c7efa4b6b
 	github.com/miekg/dns v1.1.1 // indirect
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20190102171810-8d7daa0c54b3 // indirect
 	golang.org/x/net v0.0.0-20181220203305-927f97764cc3 // indirect
 	golang.org/x/sys v0.0.0-20190102155601-82a175fd1598 // indirect
--- sip/sip_test.go
+++ sip/sip_test.go
@@ -6,22 +6,23 @@ import (
 	"testing"
 
 	"github.com/go-test/deep"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestInvitationCodec(t *testing.T) {
+	// given
 	msg := ControlMessage{
 		Cmd:   Invitation,
 		Token: 12345,
 		SSRC:  54321,
 		Name:  "foo-bar",
 	}
-	buffer := Encode(msg)
-	actual, _ := Decode(buffer)
-
+	// when
+	actual, err := Decode(Encode(msg))
+	// then
 	fmt.Println(hex.Dump([]byte(msg.Name)))
-
+	assert.Nil(t, err)
 	if diff := deep.Equal(msg, actual); diff != nil {
 		t.Error(diff)
 	}
-
 }
